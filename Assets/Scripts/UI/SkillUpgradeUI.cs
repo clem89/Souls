d52a6fa95dc5132a6b76b8a479449b64b15f5a58
clem <ldh5409@gmail.com>
2026-06-03 12:36:04 +0900

    fix: asmdef에 Unity.TextMeshPro 참조 추가, SkillUpgradeUI TMP_Text로 전환

--- Assets/Scripts/UI/SkillUpgradeUI.cs
+++ Assets/Scripts/UI/SkillUpgradeUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,11 +6,12 @@ public class SkillUpgradeUI : MonoBehaviour
 {
     public static SkillUpgradeUI Instance { get; private set; }
 
-    [SerializeField] GameObject _panel;
-    [SerializeField] Button[]   _cardButtons;    // 길이 3
-    [SerializeField] Text[]     _cardNameTexts;  // 길이 3
-    [SerializeField] Text[]     _cardDescTexts;  // 길이 3
-    [SerializeField] Text[]     _cardCoeffTexts; // 길이 3
+    [SerializeField] GameObject      _panel;
+    [SerializeField] Button[]        _cardButtons;    // 길이 3
+    [SerializeField] TMP_Text[]      _cardNameTexts;  // 길이 3
+    [SerializeField] TMP_Text[]      _cardDescTexts;  // 길이 3
+    [SerializeField] TMP_Text[]      _cardCoeffTexts; // 길이 3
+    
 
     string           _pendingBaseSkillId;
     PlayerSkillState _pendingState;
