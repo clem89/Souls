670a89bf050932e501f7a02920e7e19cc6b4ab02
clem <ldh5409@gmail.com>
2026-06-03 10:44:55 +0900

    feat: SkillDef에 nextSkillIds 추가, coeffPerLevel 제거

--- Assets/Scripts/Data/SkillDef.cs
+++ Assets/Scripts/Data/SkillDef.cs
@@ -3,8 +3,8 @@ using System;
 [Serializable]
 public class SkillDef
 {
-    public string skillId;
-    public string effectId;
-    public float  baseCoefficient;
-    public float  coeffPerLevel;
+    public string   skillId;
+    public string   effectId;
+    public float    baseCoefficient;
+    public string[] nextSkillIds;
 }
