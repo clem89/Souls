e18cd6f58e107df2145b28ce88966b10b4848b8f
clem <ldh5409@gmail.com>
2026-06-03 10:49:07 +0900

    feat: SkillUpgradeUI 카드 선택 UI 추가

---
+++ Assets/Scripts/UI/SkillUpgradeUI.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillUpgradeUI : MonoBehaviour
+{
+    public static SkillUpgradeUI Instance { get; private set; }
+
+    [SerializeField] GameObject _panel;
+    [SerializeField] Button[]   _cardButtons;    // 길이 3
+    [SerializeField] Text[]     _cardNameTexts;  // 길이 3
+    [SerializeField] Text[]     _cardCoeffTexts; // 길이 3
+
+    string           _pendingBaseSkillId;
+    PlayerSkillState _pendingState;
+
+    void Awake()
+    {
+        Instance = this;
+        _panel.SetActive(false);
+    }
+
+    public void Show(string baseSkillId, SkillDef[] options, PlayerSkillState skillState)
+    {
+        _pendingBaseSkillId = baseSkillId;
+        _pendingState       = skillState;
+        Time.timeScale      = 0f;
+        _panel.SetActive(true);
+
+        for (int i = 0; i < _cardButtons.Length; i++)
+        {
+            bool active = i < options.Length;
+            _cardButtons[i].gameObject.SetActive(active);
+            if (!active) continue;
+
+            var opt = options[i];
+            _cardNameTexts[i].text  = opt.skillId;
+            _cardCoeffTexts[i].text = $"계수 x{opt.baseCoefficient:F1}";
+
+            int captured = i;
+            _cardButtons[i].onClick.RemoveAllListeners();
+            _cardButtons[i].onClick.AddListener(() => OnCardSelected(options[captured].skillId));
+        }
+    }
+
+    void OnCardSelected(string chosenSkillId)
+    {
+        _pendingState?.Evolve(_pendingBaseSkillId, chosenSkillId);
+        _panel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
